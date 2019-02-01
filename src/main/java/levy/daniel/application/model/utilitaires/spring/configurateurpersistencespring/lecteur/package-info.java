b0f13ea63e9701404b17b14b6fdf382a979a3eb8
daniel.levy <daniel.levy@cerema.fr>
2019-02-01 11:35:13 +0100

    COMMIT_&_PUSH_DL_BUREAU

--- src/main/java/levy/daniel/application/model/utilitaires/spring/configurateurpersistencespring/lecteur/package-info.java
+++ src/main/java/levy/daniel/application/model/utilitaires/spring/configurateurpersistencespring/lecteur/package-info.java
@@ -4,7 +4,16 @@
  * pour la <b>lecture des fichiers properties de configuration 
  * d'une base de données par SPRING</b> 
  * en Java.<br/>
- * <br/>
+
+ * 
+ * <p>
+ * <b><span style="text-decoration: underline;">
+ * OBJECTIF du lecteur de configuration SPRING :
+ * </span></b>
+ * </p>
+ * <p>
+ * Préparer les propriétés stockées dans un fichier properties SPRING pour instancier un EntityManagerFactory.<br/>
+ * </p>
  * 
  * <p>
  * <b><span style="text-decoration: underline;">
@@ -12,7 +21,31 @@
  * </span></b>
  * </p>
  * <p>
- * 
+ * un <code>org.springframework.core.env.Environment</code> 
+ * est automatiquement instancié 
+ * lors de la configuration du CONTEXTE SPRING au lancement de l'application.<br/>
+ * Cet Environment lit automatiquement le fichier properties SPRING désigné dans 
+ * l'annotation PropertySource au dessus de la classe de configuration SPRING et l'encapsule.<br/>
+ * Les propriétés stockées dans le fichier properties SPRING sont des propriétés SPRING 
+ * qui ne peuvent être utilisées telles quelles pour instancier 
+ * un <code>EntityManagerFactory</code>.<br/>
+ * par exemple :
+ * <ul>
+ * <li><code>spring.jpa.properties.hibernate.dialect</code> 
+ * devient <code>hibernate.dialect</code> dans un EntityManagerFactory
+ *  (pour un PersistenceProvider HIBERNATE).</li>
+ * <li><code>spring.jpa.properties.hibernate.ddl-auto</code> 
+ * devient <code>hibernate.hbm2ddl.auto</code> dans un EntityManagerFactory
+ *  (pour un PersistenceProvider HIBERNATE).</li>
+ * </ul>
+ * <b>Le LecteurConfigurationBaseSpring prépare toutes les properties 
+ * pour qu'elle soient directement injectables dans un 
+ * <code>org.springframework.orm.jpa.persistenceunit.MutablePersistenceUnitInfo</code> 
+ * (utilisé ensuite pour instancier un <code>EntityManagerFactory</code>)</b>.
+ * </p>
+ * <p>
+ * <img src="../../../../../../../../../../..//javadoc/images/model/utilitaires/spring/configurateurpersistencespring/lecteur/fonctionnement_lecteurconfigurationbase.png" 
+ * alt="fonctionnement des lecteurs de configuration de BD SPRING" border="1" align="center" />
  * </p>
  * 
  * <p>
@@ -30,6 +63,10 @@
  * <br/><br/>
  * 
  * - Exemple d'utilisation :<br/>
+ * <code>
+ *  // INSTANCIATION DU LecteurConfigurationBaseSpring DEPUIS LE SETTER AUTOWIRED setEnvironmentSpring(...) DE LA CLASSE DE CONFIGURATION SPRING<br/>
+ *  this.setLecteurConfigurationBaseSpring(new LecteurConfigurationBaseSpring(this.environmentSpring));<br/>
+ * </code>
  *<br/>
  * 
  * - Mots-clé :<br/>
